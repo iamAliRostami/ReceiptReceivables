c963064ff14b86aa232f2ea1d12980074f18869e
Leon <ali.rostami33@gmail.com>
2020-11-23 12:48:00 +0330

    initialize commit

---
+++ app/src/main/java/com/leon/receipt_receivables/MainActivity.java
@@ -0,0 +1,13 @@
+package com.leon.receipt_receivables;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
